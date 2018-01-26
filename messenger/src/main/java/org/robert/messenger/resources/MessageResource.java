fa13280056b2f1c1173d1c1c2e3e6e98145832cc
robertlemon <robertlemon@outlook.com>
2018-01-26 10:22:59 +0100

    Update/delete

--- messenger/src/main/java/org/robert/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/robert/messenger/resources/MessageResource.java
@@ -2,8 +2,13 @@ package org.robert.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,8 +21,38 @@ public class MessageResource {
 	MessageService service = new MessageService();
 
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return service.getAllMessages();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return service.addMessage(message);	
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return service.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long messageId) {
+		service.removeMessage(messageId);
+	}
+
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return service.getMessage(messageId);
 	}
 }
